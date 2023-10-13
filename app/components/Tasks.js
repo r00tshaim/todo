e39882d0bd410fb908fd75348c8d6bffaecfc1d2
Shaim Khanusiya <shaim456@gmail.com>
2023-10-13 10:19:03 +0530

    delete feature

--- app/components/Tasks.js
+++ app/components/Tasks.js
@@ -1,32 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
 
-function Tasks({ tasks }) {
-
-  const deleteTaskHandler = async (taskId) => {
-    try {
-      console.log(`deleteTaskHandler called for taskid=${taskId} type=${typeof taskId}`)
-      const res = await fetch(`/api/task/${taskId}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-type': 'applicaton/json',
-          Accept: 'applicaton/json'
-        },
-      });
-
-      // Call .json() on the response to get the JSON body
-      //const data = await res.json(); // <-- Add this line
-      console.log(`deleteTaskHandler res=${res.json()}`)
-  
-      if (res.ok) {
-        console.log(`Task deleted with id=${taskId}`);
-      } else {
-        console.log(`Task deletion failed with id=${taskId}`);
-      }
-    } catch (err) {
-      console.log(`Error while deleting task with id=${taskId} DELETE call error ${err}`);
-    }
-  };
+function Tasks({ tasks, deleteTaskHandler }) {
 
   const { t } = useTranslation();
   return (
